4f22a09d190efc4f0790623999b5ad9b210d250a
mosesrc <ryanchristophermoses@gmail.com>
2022-09-29 18:02:15 -0400

    Form Validation Setup Complete and Working

---
+++ src/main/java/com/tts/formInputValidation/FormInputValidationApplication.java
@@ -0,0 +1,13 @@
+package com.tts.formInputValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormInputValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormInputValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/formInputValidation/FormInputValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.formInputValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormInputValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
